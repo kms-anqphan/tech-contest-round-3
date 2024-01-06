c941fc5be4f6c84378092ef26595e99d6fd14579
dangnnguyen <dang.nguyen@leasequery.com>
2024-01-06 17:29:14 +0700

    add buttons

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import EventService from '../../services/EventService';
 import moment from 'moment';
+import { useNavigate } from 'react-router';
 
 import './Home.css';
 
 export default function Home() {
 	const [events, setEvents] = useState([]);
+	const navigate = useNavigate();
 	useEffect(() => {
 		// set events
 		EventService.getAllEvent()
@@ -14,6 +16,14 @@ export default function Home() {
 			})
 			.catch((err) => console.log(err));
 	})
+
+	const handleCreateEvent = () => {
+		navigate('/event_entry', { replace: true });
+	}
+
+	const handleCreateItem = () => {
+		navigate('/item_entry', { replace: true });
+	}
 
 	// make a list of simple card components with event details
 	const eventList = events.map((event) => {
@@ -31,6 +41,8 @@ export default function Home() {
     <div className='home-container'>
       <h1>Welcome to the Event Management System</h1>
       <p>Here you can manage all the events created by the company.</p>
+			<button className='create-event-button' onClick={handleCreateEvent}>Create Event</button>
+			<button className='create-item-button' onClick={handleCreateItem}>Create Item</button>
 			{eventList}
     </div>
   );
