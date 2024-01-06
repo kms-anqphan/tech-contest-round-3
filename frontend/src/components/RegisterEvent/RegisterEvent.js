30f58014c18c91307e6678017633d903749cd1cd
An Quoc Phan <anqphan@kms.com.vn>
2024-01-06 17:20:19 +0700

    change base URL

--- frontend/src/components/RegisterEvent/RegisterEvent.js
+++ frontend/src/components/RegisterEvent/RegisterEvent.js
@@ -1,13 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import EventService from "../../services/EventService";
 
 export default function RegisterEvent() {
   const { eventId } = useParams();
+  const [event, setEvent] = useState({});
 
   useEffect(() => {
-    EventService.getEventById(eventId)
+    EventService.getEventById(eventId).then((res) => setEvent(res.data));
   }, [eventId]);
 
-  return <div>Register Event</div>;
+  return (
+    <div>
+      <h1>Register Event</h1>
+      <h2>Event Details:</h2>
+      <p>Event Name: {event.name}</p>
+      <p>Event Date: {event.date}</p>
+      <p>Event Location: {event.location}</p>
+      {/* Add more event details as needed */}
+    </div>
+  );
 }
