a5cc067f3591eb83e790f003a12b8be9fbb9f8d5
dangnnguyen <dang.nguyen@leasequery.com>
2024-01-06 14:27:30 +0700

    change server port

--- server/App.js
+++ server/App.js
@@ -9,7 +9,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 const bodyParser = require('body-parser');
 
 const eventRouter = require('./routes/event');
